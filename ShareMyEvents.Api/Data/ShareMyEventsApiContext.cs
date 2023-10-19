c6e1ae6bb277940b3731b0cb64757952c128b5b9
jerkoWinLegion <jerome.potie@gmail.com>
2023-10-20 00:52:37 +0200

    Create Db context

---
+++ ShareMyEvents.Api/Data/ShareMyEventsApiContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShareMyEvents.Domain.Models;
+
+namespace ShareMyEvents.Api.Data
+{
+    public class ShareMyEventsApiContext : DbContext
+    {
+        public ShareMyEventsApiContext (DbContextOptions<ShareMyEventsApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; } = default!;
+        public DbSet<Participation> Participations { get; set; } = default!;
+        public DbSet<Address> Addresses { get; set; } = default!;
+        public DbSet<Actor> Actors { get; set; } = default!;
+        public DbSet<Availability> Availabilities{ get; set; } = default!;
+    }
+}
