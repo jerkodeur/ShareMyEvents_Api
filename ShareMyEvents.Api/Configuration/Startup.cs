b771f8148153cc8129d137ce50933c88c98dc37d
jerkoWinLegion <jerome.potie@gmail.com>
2023-10-22 15:19:37 +0200

    Create a Startup class to contains startup application configuration

---
+++ ShareMyEvents.Api/Configuration/Startup.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using ShareMyEvents.Api.Data;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace ShareMyEvents.Api.Configuration;
+internal class Startup
+{
+    private static WebApplicationBuilder _builder;
+    private static DbContext _smeDbContext;
+
+    public static void Initialize (string[] args)
+    {
+        if (_builder == null)
+        {
+            _builder = WebApplication.CreateBuilder(args);
+        }
+
+        RegisterServices ();
+        Configure();
+    }
+
+    private static void Configure ()
+    {
+        var app = _builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if(app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+        app.UseAuthentication();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+
+    private static void RegisterServices()
+    {
+        _builder?.Host.ConfigureServices((context, services) =>
+        {
+            // DbContext
+            RegisterDbContextService(services);
+
+            services.AddControllers();
+
+            // Swagger
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            // Authentication with JWT
+            RegisterAuthenticationService(services, context);
+        });
+    }
+
+    private static void RegisterDbContextService(IServiceCollection services)
+    {
+        services.AddDbContext<ShareMyEventsApiContext>(dbContextOptions =>
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var connectionString = configuration.GetConnectionString("ShareMyEventsApiContext") ?? throw new InvalidOperationException("Connection string 'ShareMyEventsApiContext' not found.");
+            dbContextOptions.UseSqlServer(connectionString);
+        });
+    }
+
+    private static void RegisterAuthenticationService(IServiceCollection services, HostBuilderContext context)
+    {
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            //options.SaveToken = true;
+            options.TokenValidationParameters = new()
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(context.Configuration["Jwt:Secret"])),
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+
+        });
+    }
+}
--- ShareMyEvents.Api/Program.cs
+++ ShareMyEvents.Api/Program.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using ShareMyEvents.Api.Data;
+﻿using ShareMyEvents.Api.Configuration;
 
 namespace ShareMyEvent.Api;
 
@@ -7,33 +6,6 @@ public class Program
 {
     public static void Main (string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddDbContext<ShareMyEventsApiContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("ShareMyEventsApiContext") ?? throw new InvalidOperationException("Connection string 'ShareMyEventsApiContext' not found.")));
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if(app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
-        app.Run();
+        Startup.Initialize(args);
     }
 }
